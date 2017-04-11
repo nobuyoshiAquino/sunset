a2c6d524d5b2aa99583e2dde9660ce6324d479ef
Nobuyoshi Aquino <nobuyoshi.aquino@gmail.com>
2017-04-11 18:07:46 -0400

    Create a sunset landscape with views

---
+++ app/src/main/java/xyz/nobu/sunset/SunsetActivity.java
@@ -0,0 +1,12 @@
+package xyz.nobu.sunset;
+
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/xyz/nobu/sunset/SunsetFragment.java
@@ -0,0 +1,24 @@
+package xyz.nobu.sunset;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nobu on 4/11/17.
+ */
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sunset_fragment, container, false);
+        return view;
+    }
+}
